906cf19b307928ec38b428d2b063fa0b81c751fb
Shivank Anchal <anchalshivank99@gmail.com>
2024-09-05 09:37:29 +0530

    Time: 6 ms (76.34%) | Memory: 2.3 MB (49.27%) - LeetSync

---
+++ 55-jump-game/jump-game.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn can_jump(nums: Vec<i32>) -> bool {
+    
+        let mut max_reach = 0;
+
+        for ( i , &num) in nums.iter().enumerate(){
+            if i > max_reach as usize{
+                return false;
+            }
+
+            max_reach = max_reach.max(i as i32 +num);
+
+            if max_reach as usize >= nums.len()-1{
+                return true;
+            }
+        }
+        false
+
+    }
+}
