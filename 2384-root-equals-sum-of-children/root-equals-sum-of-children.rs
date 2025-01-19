85c758f96ac2d04dd55b16581c672cde04e85cfe
Shivank Anchal <anchalshivank99@gmail.com>
2025-01-19 23:00:26 +0530

    Time: 0 ms (100.00%) | Memory: 2.2 MB (37.78%) - LeetSync

---
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.rs
@@ -0,0 +1,80 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+type A = Option<Rc<RefCell<TreeNode>>>;
+impl Solution {
+    pub fn check_tree(root: A) -> bool {
+        
+        fn helper(node: A) -> (i32, bool){
+
+            match node{
+                None => (0,true),
+                Some(n) => {
+
+                    let nb = n.borrow();
+
+                    if nb.left.is_none() && nb.right.is_none(){
+                        return (nb.val, true);
+                    }
+
+                    let (left_sum , left_valid)  = if nb.left.is_none(){
+                        (0, true)
+                    }else{
+                        helper(nb.left.clone())
+                    };
+
+                    if !left_valid{
+                        println!("left not valid");
+                        return (0, false);
+                    }
+
+                    let (right_sum , right_valid) = if nb.right.is_none(){
+                        (0, true)
+                    }else{
+
+                        helper(nb.right.clone())
+
+                    };
+
+                    if !right_valid {
+                        println!("right not valiud");
+                        return (0, false);
+                    }
+
+                    println!("{} = {} + {}", nb.val, left_sum , right_sum);
+
+                    if nb.val == left_sum + right_sum {
+                        println!("SUm is valid");
+                        (left_sum+ right_sum + nb.val, true)
+                    }else{
+                            println!("sum not vald");
+                        (0, false)
+                    }
+
+                }
+            }
+
+        }
+
+        helper(root).1
+        
+
+    }
+}
