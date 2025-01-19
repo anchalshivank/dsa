4a4093e597f94168aa6b043c7b4b124632a0feb9
Shivank Anchal <anchalshivank99@gmail.com>
2025-01-19 23:57:23 +0530

    Time: 3 ms (87.61%) | Memory: 2.3 MB (92.92%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.rs
+++ 1250-longest-common-subsequence/longest-common-subsequence.rs
@@ -1,36 +1,30 @@
 impl Solution {
     pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
+        let t1: Vec<char> = text1.chars().collect();
+        let t2: Vec<char> = text2.chars().collect();
 
-
-        let t1 : Vec<char>= text1.chars().collect();
-        let t2 : Vec<char>= text2.chars().collect();
-        // in simple terms lcs[0, 0] = 
-        //if equal then 1+lcs[1,1]
-        //else
-        // max(lcs[0,1] , lcs[1,0])
-        //but we know the value of last n 
-        //so we will do from bottom 
         let len1 = t1.len();
         let len2 = t2.len();
 
-        let mut lcs = vec![vec![0; len2+1]; len1+1];
-    
-        for (i, ch1 ) in t1.iter().enumerate(){
-            for (j, ch2) in t2.iter().enumerate(){
+        // Use two arrays for DP: previous row and current row
+        let mut prev = vec![0; len2 + 1];
+        let mut current = vec![0; len2 + 1];
 
-                if ch1 == ch2 {
-                    //it means it is equal 
-                    //the lcs shall be
-                    lcs[i+1][j+1] = lcs[i][j] + 1;
-                }else{
-
-                    //we will just take the max of any 
-                    lcs[i+1][j+1] = lcs[i+1][j].max(lcs[i][j+1]);
-
+        for i in 0..len1 {
+            for j in 0..len2 {
+                if t1[i] == t2[j] {
+                    // If characters match, increment the length from the diagonal (prev[j] + 1)
+                    current[j + 1] = prev[j] + 1;
+                } else {
+                    // Otherwise, take the max of the left or top value
+                    current[j + 1] = current[j].max(prev[j + 1]);
                 }
-
             }
-        }        
-        lcs[len1][len2]
+            // After finishing the current row, copy it to the prev row for the next iteration
+            prev.copy_from_slice(&current);
+        }
+
+        // The result is in the last cell of the current row (current[len2])
+        current[len2]
     }
 }
