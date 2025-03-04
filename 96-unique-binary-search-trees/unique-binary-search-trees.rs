91930122f195e4d3890689583de7854a37672ad2
Shivank Anchal <anchalshivank99@gmail.com>
2025-03-04 13:43:46 +0530

    Time: 0 ms (100.00%) | Memory: 2.3 MB (5.00%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.rs
+++ 96-unique-binary-search-trees/unique-binary-search-trees.rs
@@ -1,12 +1,19 @@
 impl Solution {
-    pub fn num_trees(n: i32) -> i32 {
-        let mut catalan: i64 = 1;
 
-        for i in 0..n {
-            // Ensure the result of the multiplication is cast to i64
-            catalan = catalan * (2 * (2 * i + 1)) as i64 / (i + 2) as i64;
+
+
+    pub fn num_trees(n: i32) -> i32 {
+
+        let mut catalan : i64 = 1;
+
+        for i in 0..n{
+
+            catalan = catalan *(2*(2*i+1)) as i64 /(i+2) as i64;
+
         }
 
         catalan as i32
+
+
     }
 }
