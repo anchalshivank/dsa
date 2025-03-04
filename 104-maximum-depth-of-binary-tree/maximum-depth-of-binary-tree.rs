04668a815e77a06d7cea38cddb19de42d48fed18
Shivank Anchal <anchalshivank99@gmail.com>
2025-03-04 21:26:54 +0530

    Time: 0 ms (100.00%) | Memory: 2.8 MB (40.68%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.rs
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.rs
@@ -24,15 +24,12 @@ impl Solution {
         fn depth(node: Option<Rc<RefCell<TreeNode>>>, current: i32) -> i32 {
 
             if let Some(node_rc) = node{
+                
                 let nb = node_rc.borrow();
                 let lh = depth(nb.left.clone(), current+1);
                 let rh = depth(nb.right.clone(), current+1);
-
                 let max_h = lh.max(rh);
-
                 max_h
-
-                
 
             }else{
                 current
