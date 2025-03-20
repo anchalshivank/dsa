db419e697eb3c3a846506e3f19b005a2ce3855fe
Shivank Anchal <anchalshivank99@gmail.com>
2025-03-20 12:13:34 +0530

    Time: 13 ms (57.14%) | Memory: 2.3 MB (85.71%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.rs
+++ 264-ugly-number-ii/ugly-number-ii.rs
@@ -1,54 +1,32 @@
 use std::collections::{BinaryHeap, HashSet};
-use std::cmp::Ordering;
-#[derive(Debug, PartialEq, Eq)]
-struct Num(i64);
-
-impl PartialOrd for Num {
-
-    fn partial_cmp(&self, other: &Num) -> Option<Ordering> {
-
-        Some(self.cmp(&other))
-
-    }
-
-}
-
-impl Ord for Num {
-
-     fn cmp(&self, other: &Num) -> Ordering {
-
-        other.0.cmp(&self.0)
-
-     }
-
-}
-
+use std::cmp::Reverse;
 impl Solution {
     pub fn nth_ugly_number(n: i32) -> i32 {
-        
+
+        //we need  heap with top nth ugly number which means min heap to get the number
+        //if current ugly number is less than the top of min heap .. than that needs to be changed
+        //pushing will makes sense .. but poping will only pop the least element 
+
         let mut bp = BinaryHeap::new();
-
         let mut seen = HashSet::new();
-        let mut un = 1;
 
-        bp.push(Num(un));
+        let mut un = 1 as i64;
+        bp.push(Reverse(un));
+
         seen.insert(un);
 
+        //it shall bne a min heap
         for _ in 0..n {
 
-            if let Some(val) = bp.pop(){
+            if let Some(Reverse(val)) = bp.pop(){
 
-                un = val.0;
+                un = val;
 
-                let next_vals = [2*val.0, 3*val.0, 5*val.0];
+                let next_vals = [2*val, 3*val, 5*val];
                 for next in next_vals{
-
                     if seen.insert(next){
-
-                        bp.push(Num(next));
-
+                        bp.push(Reverse(next));
                     }
-
                 }
 
             }
@@ -57,5 +35,7 @@ impl Solution {
 
         un as i32
 
+
+        
     }
 }
