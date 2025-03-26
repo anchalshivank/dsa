ede62b36a0b62bf57db08f2da96775b4fcd23219
Shivank Anchal <anchalshivank99@gmail.com>
2025-03-26 20:13:32 +0530

    Time: 0 ms (100.00%) | Memory: 3.2 MB (97.65%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.rs
@@ -0,0 +1,35 @@
+impl Solution {
+    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+        let mut flag = false;
+
+        let mut count = 0;
+        
+        let product: i32 = nums.iter().filter(|&x| if *x == 0{
+            flag = true;
+            count+=1;
+            false
+        }else{
+            true
+        }).product();
+        println!("{count}");
+        
+        let mut result = Vec::new();
+        for i in 0..nums.len(){
+            if count > 1{
+                result.push(0);
+            }
+            else if nums[i] == 0{
+                result.push(product);
+            }else{
+                if flag{
+                    result.push(0);
+                }else{
+
+                    result.push(product/nums[i]);
+                }
+            }
+
+        }
+        result
+    }
+}
