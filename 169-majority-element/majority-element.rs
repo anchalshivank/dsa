653b3eba0adef64e19f754c5f090788167f59a0b
Shivank Anchal <anchalshivank99@gmail.com>
2025-03-26 15:06:15 +0530

    Time: 0 ms (100.00%) | Memory: 2.3 MB (100.00%) - LeetSync

--- 169-majority-element/majority-element.rs
+++ 169-majority-element/majority-element.rs
@@ -1,22 +1,26 @@
 impl Solution {
     pub fn majority_element(nums: Vec<i32>) -> i32 {
-        let mut mj = nums[0];
+        
+        let mut maj = nums[0];
+
         let mut count = 1;
-        for i in 1..nums.len(){
-            let num = nums[i];
-            if count == 0 {
-                mj = num;
-            }
-            if mj == num{
+
+        for &num in nums[1..].iter(){
+
+            if num == maj{
                 count+=1;
             }else{
                 count -=1;
             }
+            // println!("{count}");
+            if count == -1{
+                maj = num;
+                count = 1;
+            }
 
-            
         }
-    
-        mj
+
+        maj
 
     }
 }
