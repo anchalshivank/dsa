72c14b6e5df41e783c4935c6b7a0bbad2084df7a
Shivank Anchal <anchalshivank99@gmail.com>
2025-03-26 09:22:15 +0530

    Time: 0 ms (100.00%) | Memory: 3.1 MB (28.36%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.rs
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.rs
@@ -1,15 +1,27 @@
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
-        let (mut buy, mut sell) = (prices[0], 0);
 
-        for &price in &prices[1..] {
-            if price < buy {
-                buy = price;
-            } else if price - buy > sell {
-                sell = price - buy;
+        let mut buy_at = prices[0];
+
+        let mut max_p = i32::MIN;
+
+        for (index, curr_sell_at)  in prices[1..].iter().enumerate(){
+            let i = index+1;
+            if *curr_sell_at > buy_at {
+
+                //means we can sell at 
+                let profit = *curr_sell_at - buy_at;
+                max_p = max_p.max(profit);
+
+
+            }else{
+                buy_at = *curr_sell_at;
             }
         }
-
-        return sell;
+        if max_p == i32::MIN{
+            return 0;            
+        }
+        max_p
+        
     }
 }
