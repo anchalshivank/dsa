bc0e54fd547e257492cfd47228d572d752fe0b9b
Shivank Anchal <anchalshivank99@gmail.com>
2025-03-26 09:11:07 +0530

    Time: 0 ms (100.00%) | Memory: 2.5 MB (33.59%) - LeetSync

--- 1-two-sum/two-sum.rs
+++ 1-two-sum/two-sum.rs
@@ -1,29 +1,27 @@
 use std::collections::HashMap;
-
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
 
-        let n = nums.len();
-
         let mut map = HashMap::new();
 
-        for (i , &num) in nums.iter().enumerate(){
-            map.insert(num, i );
+        for (index, num) in nums.iter().enumerate(){
+
+            map.insert(num , index as i32);
+
         }
 
+        for (index, num ) in nums.iter().enumerate(){
+            let to_find = target-num;
+            if map.contains_key(&to_find){
+                let index2 = *map.get(&to_find).unwrap();
+                if index as i32 !=index2{
 
-        for (i , &num) in nums.iter().enumerate(){
-            let complement = target-num;
-            if let Some(&j) = map.get(&complement){
-                if i!=j{
-                    return vec![i as i32, j as i32];
+                return vec![index2, index as i32];
                 }
             }
         }
 
         vec![]
-
-        
         
     }
 }
