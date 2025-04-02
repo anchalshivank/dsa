5c91c970823c1d2406dd39986362fb1c6191766a
Shivank Anchal <anchalshivank99@gmail.com>
2025-04-02 12:10:26 +0530

    Time: 3 ms (45.88%) | Memory: 9.4 MB (65.88%) - LeetSync

---
+++ 221-maximal-square/maximal-square.rs
@@ -0,0 +1,26 @@
+impl Solution {
+    pub fn maximal_square(matrix: Vec<Vec<char>>) -> i32 {
+        
+        let n = matrix.len();
+        let m = matrix[0].len();
+
+        let mut dp = vec![vec![0;m+1]; n+1];
+
+        let mut a = i32::MIN;
+
+        for i in 1..=n{
+            for j in 1..=m{
+
+                //max square that can be formed of any length is a 
+                //Now lets compare 
+                if matrix[i-1][j-1] == '1'{
+                    dp[i][j] = i32::min(i32::min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1])+1;
+                    a = a.max(dp[i][j]);
+                }
+            }
+        }
+
+        // println!("{:?}", dp);
+        a*a
+    }
+}
