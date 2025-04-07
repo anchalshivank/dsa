707f49a8f7cf66a24bee6a9f60c3772590bffaa3
Shivank Anchal <anchalshivank99@gmail.com>
2025-04-07 18:07:58 +0530

    Time: 0 ms (100.00%) | Memory: 2.1 MB (100.00%) - LeetSync

--- 1040-maximum-binary-tree-ii/maximum-binary-tree-ii.rs
+++ 1040-maximum-binary-tree-ii/maximum-binary-tree-ii.rs
@@ -19,31 +19,40 @@
 use std::rc::Rc;
 use std::cell::RefCell;
 
-type A = Rc<RefCell<TreeNode>>;
+type A  = Rc<RefCell<TreeNode>>;
 
 impl Solution {
     pub fn insert_into_max_tree(root: Option<A>, val: i32) -> Option<A> {
-        fn helper(node: Option<A>, val: i32) -> Option<A> {
-            if let Some(node_rc) = node {
-                {
-                    let mut node_bm = node_rc.borrow_mut();
-                    if node_bm.val > val {
-                        node_bm.right = helper(node_bm.right.clone(), val);
-                        return Some(node_rc.clone()); 
-                    }
-                } // now the borrow is fully dropped
 
-                Some(Rc::new(RefCell::new(TreeNode {
-                    val,
-                    left: Some(node_rc.clone()), 
-                    right: None,
-                })))
-            } else {
+        fn helper(node: Option<A>, val: i32) -> Option<A>{
+
+            if let Some(node_rc) = node{
+
+                let mut node_bm = node_rc.borrow_mut();
+
+                if node_bm.val > val{
+
+                        //send the thing to right
+                    node_bm.right = helper(node_bm.right.clone(), val);
+
+                    return Some(node_rc.clone());
+
+                }
+
+                drop(node_bm);
+                //means our val > bode_bm.val
+                let mut n = TreeNode::new(val);
+                n.left = Some(node_rc);
+                Some(Rc::new(RefCell::new(n)))
+            }else{
+                //just return the val
                 Some(Rc::new(RefCell::new(TreeNode::new(val))))
             }
+
         }
 
-
         helper(root, val)
+
+
     }
 }
