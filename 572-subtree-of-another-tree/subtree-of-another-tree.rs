b4d63b6da2dac15e8745c3ee8695dbbdc76a0447
Shivank Anchal <anchalshivank99@gmail.com>
2025-04-07 21:32:42 +0530

    Time: 2 ms (27.59%) | Memory: 2.3 MB (68.97%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.rs
+++ 572-subtree-of-another-tree/subtree-of-another-tree.rs
@@ -1,94 +1,37 @@
-// Definition for a binary tree node.
-// #[derive(Debug, PartialEq, Eq)]
-// pub struct TreeNode {
-//   pub val: i32,
-//   pub left: Option<Rc<RefCell<TreeNode>>>,
-//   pub right: Option<Rc<RefCell<TreeNode>>>,
-// }
-// 
-// impl TreeNode {
-//   #[inline]
-//   pub fn new(val: i32) -> Self {
-//     TreeNode {
-//       val,
-//       left: None,
-//       right: None
-//     }
-//   }
-// }
 use std::rc::Rc;
 use std::cell::RefCell;
+
 type A = Rc<RefCell<TreeNode>>;
 
 impl Solution {
     pub fn is_subtree(root: Option<A>, sub_root: Option<A>) -> bool {
-
-        //we need to confirm
-        //let's traverse the tree
-
-        fn helper(node: Option<A>, sub: Option<A>) -> bool {
-    match (node, sub) {
-        (Some(node_rc), Some(sub_rc)) => {
-            let node_b = node_rc.borrow();
-            let sub_b = sub_rc.borrow();
-
-            node_b.val == sub_b.val
-                && helper(node_b.left.clone(), sub_b.left.clone())
-                && helper(node_b.right.clone(), sub_b.right.clone())
-        }
-        (None, None) => true,
-        _ => false,
-    }
-}
-
-
-        fn pre(node: Option<A> , sub: Option<A>) -> bool{
-            
-            //we need to confirm that sub exist or not!!
-            match (node, sub){
-                (Some(node_rc), Some(sub_rc)) => {
-                    let node_b = node_rc.borrow();
-                    
-                    let sub_b = sub_rc.borrow();
-
-                    if node_b.val == sub_b.val{
-
-                        //we need to check that this is equal or not!
-                        //once we reach here just compare recurssivedly
-
-                        if !helper(Some(node_rc.clone()), Some(sub_rc.clone())){
-                            if pre(node_b.left.clone(), Some(sub_rc.clone())){
-                            return true;
-                        }else{
-                            pre(node_b.right.clone(), Some(sub_rc.clone()))
-                        }
-                        }else{
-                            true
-                        }
-
-                    }else{
-                        //keep traversing!!
-
-                        if pre(node_b.left.clone(), Some(sub_rc.clone())){
-                            return true;
-                        }else{
-                            pre(node_b.right.clone(), Some(sub_rc.clone()))
-                        }
-                        
-
-                    }
-
+        // Compare if two trees are exactly the same
+        fn is_same(tree1: Option<A>, tree2: Option<A>) -> bool {
+            match (tree1, tree2) {
+                (Some(a), Some(b)) => {
+                    let (a, b) = (a.borrow(), b.borrow());
+                    a.val == b.val
+                        && is_same(a.left.clone(), b.left.clone())
+                        && is_same(a.right.clone(), b.right.clone())
                 }
-                _ => false
+                (None, None) => true,
+                _ => false,
             }
-
-
         }
 
+        // Traverse the main tree and check for subtree match
+        fn traverse(node: Option<A>, sub: Option<A>) -> bool {
+            match node {
+                Some(n) => {
+                    let n_borrow = n.borrow();
+                    is_same(Some(n.clone()), sub.clone())
+                        || traverse(n_borrow.left.clone(), sub.clone())
+                        || traverse(n_borrow.right.clone(), sub)
+                }
+                None => false,
+            }
+        }
 
-        pre(root, sub_root)
-
-
-        
+        traverse(root, sub_root)
     }
 }
