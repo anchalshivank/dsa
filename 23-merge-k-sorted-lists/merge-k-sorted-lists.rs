7df2039834e8ce4c40ad71c99b4d364522f83546
Shivank Anchal <anchalshivank99@gmail.com>
2025-04-12 17:30:25 +0530

    Time: 4 ms (37.37%) | Memory: 3.1 MB (97.89%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.rs
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.rs
@@ -14,76 +14,56 @@
 //     }
 //   }
 // }
+use std::collections::BinaryHeap;
+
+
+impl PartialOrd for ListNode{
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering>{
+
+        Some(self.cmp(other))
+
+    }
+}
+
+impl Ord for ListNode{
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering{
+        other.val.cmp(&self.val)
+    }
+}
+
 impl Solution {
     pub fn merge_k_lists(lists: Vec<Option<Box<ListNode>>>) -> Option<Box<ListNode>> {
         
-        //ok see I can have a res linked list and merge them 
-        //time complexity shall remain same even incase of divide and conquer
-
-        //let's create a simple function to merge a list
-
-
-        fn merge(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>>{
-
-            let mut l1 = &list1;
-            let mut l2 = &list2;
-
-            let mut dummy = Some(Box::new(ListNode::new(0)));
-            let mut curr = &mut dummy;
-
-            while let (Some(p1), Some(p2)) = (l1, l2){
-
-                let node = if p1.val< p2.val{
-                    l1 = & p1.next;
-                    Some(Box::new(ListNode::new(p1.val)))
-
-                }else{
-                    l2 = &p2.next;
-                    Some(Box::new(ListNode::new(p2.val)))
-
-                };
-
-                if let Some(ref mut c) = curr{
-
-                    c.next = node;
-                    curr = &mut c.next;
-
-                }
-
-            }
-
-            match (l1, l2){
-                (Some(p), None) | (None, Some(p)) => {
-
-                    if let Some(ref mut c) = curr{
-
-
-                        c.next = Some(p.clone());
-
-                    }
-                    
-
-                }
-                _ => {}
-            }
-
-            if let Some(d) = dummy{
-                d.next
-            }else{
-                None
-            }
-        
-        }
-
-        let mut res = None;
+        let mut bh = BinaryHeap::new();
 
         for list in lists{
+            bh.push(list);
+        }
 
-            res = merge(list, res);
+        let mut result = Some(Box::new(ListNode::new(0)));
+        let mut curr = &mut result;
+
+        while let Some(Some(node)) = bh.pop(){
+
+            let new_node = Some(Box::new(ListNode::new(node.val)));
+
+            if let Some(next_node) = node.next{
+
+                bh.push(Some(next_node));
+
+            }
+
+            if let Some(ref mut c) = curr{
+
+                c.next = new_node;
+                curr = &mut c.next;
+
+            }
 
         }
 
-        res
+        result.unwrap().next
+
 
     }
 }
