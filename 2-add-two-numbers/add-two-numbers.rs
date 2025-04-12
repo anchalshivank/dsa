48d14403a097402281954021574c82267c5603e7
Shivank Anchal <anchalshivank99@gmail.com>
2025-04-12 13:43:59 +0530

    Time: 1 ms (26.27%) | Memory: 2.3 MB (80.71%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.rs
+++ 2-add-two-numbers/add-two-numbers.rs
@@ -15,29 +15,30 @@
 //   }
 // }
 impl Solution {
-    pub fn add_two_numbers(mut l1: Option<Box<ListNode>>,mut l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        // println!("{} {}", l1.unwrap().val, l2.unwrap().val);
-        // let mut l1 = l1.clone();
-        // let mut l2 = l2.clone();
+    pub fn add_two_numbers(mut l1: Option<Box<ListNode>>, mut l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        
+        //this is that we are transferring ownership
+        //But this p1 shall be reference
 
         let mut head = Box::new(ListNode::new(0));
         let mut current = &mut head;
 
         let mut carry = 0;
 
-        while (l1!=None || l2!=None || carry!=0){
-            
-           let val = match l1{
-            Some(n) => {l1 = n.next; n.val},
-            None => 0
-           }+match l2{
-            Some(n)=>{l2 = n.next; n.val},
-            None =>0
-           }+carry;
-           carry = val/10;
-           current.next = Some(Box::new(ListNode::new(val%10)));
-           current = current.next.as_mut().unwrap();
-            
+        while (l1 != None || l2 != None || carry != 0){
+
+            let val = match l1{
+                Some(n) => {l1 = n.next; n.val},
+                None => 0
+            }+ match l2{
+                Some(n) => {l2 = n.next; n.val},
+                None => 0
+            }+carry;
+
+            carry = val/10;
+            current.next = Some(Box::new(ListNode::new(val%10)));
+            current = current.next.as_mut().unwrap();
+
         }
 
         head.next
