b00822e6c0bbfc9edf57933da852c201958abd98
Shivank Anchal <anchalshivank99@gmail.com>
2025-04-27 00:05:16 +0530

    Time: 0 ms (100.00%) | Memory: 2.4 MB (12.00%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.rs
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.rs
@@ -20,36 +20,45 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn flatten(root: &mut Option<Rc<RefCell<TreeNode>>>) {
-        //this is flattened tree node
         
-        //we shall use stack for having 
         if root.is_none(){
             return;
         }
 
         let mut stack = Vec::new();
-        if let Some(r) = root{
-            stack.push(r.clone());
-        }
-        let mut head = Rc::new(RefCell::new(TreeNode::new(0)));
-        while let Some(node) = stack.pop(){
 
-            let mut node_bm = node.borrow_mut();
+        stack.push(root.clone());
 
-            if let Some(right) = node_bm.right.take(){
-                stack.push(right);
+        // stack.push(root.clone());
+
+
+        while let Some(Some(mut curr_rc)) = stack.pop(){
+            
+            let mut curr = curr_rc.borrow_mut();
+
+            
+
+            if curr.right.is_some(){
+
+                stack.push(curr.right.take());
+
             }
 
-            if let Some(left) = node_bm.left.take(){
-                stack.push(left);
+            if curr.left.is_some(){
+                stack.push(curr.left.take());
             }
 
-            drop(node_bm);
+            curr.left = None;
 
-            head.borrow_mut().right = Some(node.clone());
-            head = node;
+            if let Some(Some(next)) = stack.last(){
+
+                curr.right = Some(next.clone());
+
+            }
+
+
 
         }
-        
+
     }
 }
