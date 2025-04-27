6469561cb2a4f88003165864b87b3bfd2e176401
Shivank Anchal <anchalshivank99@gmail.com>
2025-04-27 22:14:01 +0530

    Time: 3 ms (53.13%) | Memory: 2.2 MB (96.88%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.rs
+++ 115-distinct-subsequences/distinct-subsequences.rs
@@ -1,39 +1,21 @@
 impl Solution {
     pub fn num_distinct(s: String, t: String) -> i32 {
-
         let n = s.len();
         let m = t.len();
         let s_chars: Vec<char> = s.chars().collect();
         let t_chars: Vec<char> = t.chars().collect();
-        let mut dp = vec![vec![0; n+1]; m+1 ];
-        dp[0].fill(1);
 
-        // println!("{:?}", dp);
-        for i in 0..m{
+        let mut dp = vec![0; m + 1];
+        dp[0] = 1; // Empty t matches empty s
 
-            for j in 0..n{
-
-                if s_chars[j] == t_chars[i]{
-
-                    dp[i+1][j+1] = dp[i][j]+ dp[i+1][j];
-
-                }else{
-                    dp[i+1][j+1] = dp[i+1][j];
+        for j in 0..n {
+            for i in (0..m).rev() {
+                if s_chars[j] == t_chars[i] {
+                    dp[i + 1] += dp[i];
                 }
-
             }
-
         }
 
-        // for row in dp.iter(){
-
-        //     println!("{:?}", row);
-
-        // }
-
-
-        dp[m][n]
-
-        
+        dp[m]
     }
 }
